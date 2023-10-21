e65b5e07a1a8daf6bc40e570b61903826c3de5a6
Javier <elchinatalpf@github.com>
2023-10-20 20:59:25 -0500

    Added server code and debug models

--- models/Thought.js
+++ models/Thought.js
@@ -1,30 +1,32 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 
-const thoughtsSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280,
+const thoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    reactionSchema
-  ],
-  toJSON: {
-    virtuals: true,
-    getters: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 thoughtsSchema.virtual('reactionCount')
 .get(function () {
--- models/User.js
+++ models/User.js
@@ -1,36 +1,39 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type:String,
-    unique: true,
-    required: true,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'thought',
+        ref: "thought",
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-      ref: 'user',
-    },
+        ref: "user",
+      },
     ],
+  },
+  {
     toJSON: {
       virtuals: true,
     },
     id: false,
-});
+  }
+);
 
 userSchema.virtual('friendCount')
 .get(function () {
